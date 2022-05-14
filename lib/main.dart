6f423f28bbad02e7b8dd2c18b29914eed36af4c0
MrShafqatNadeem <shafqatnadeem2@gmail.com>
2022-05-14 21:00:34 +0500

    debug banner removed, screenshots added

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
         ],
         child: MaterialApp(
           title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
           theme: ThemeData(
             primarySwatch: Colors.blue,
             // inputDecorationTheme: const InputDecorationTheme(
